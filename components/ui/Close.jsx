bb5643550e8c7d710878dee7fb428edb5e45f03a
YogurtFusion <cocaniket2@gmail.com>
2026-04-19 14:43:58 +0530

    feat: improved search animation in top nav bar

--- components/ui/Close.jsx
+++ components/ui/Close.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CloseIcons = () => {
   return (
- <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+ <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 -960 960 960" width="17px" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
   )
 }
 
