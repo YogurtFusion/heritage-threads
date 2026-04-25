c0756074da24a9b9c200a475ad31c40e355a2832
YogurtFusion <cocaniket2@gmail.com>
2026-04-25 16:00:16 +0530

    fixed hero section

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -8,34 +8,34 @@ const Hero = () => {
   return (
     <section className=" relative h-screen md:min-h-[80vh] w-full  overflow-hidden bg-body">
       {/* wrap */}
-      <div className=" relative flex flex-col md:flex-row  justify-between items-center gap-6 w-full h-full  ">
+      <div className=" relative flex flex-col lg:flex-row  justify-between items-center gap-6  w-full h-full  ">
         {/* img */}
         <Image
           src={HeroImg}
-          className=" hidden md:block max-w-2/3 w-full h-full object-cover"
+          className=" hidden lg:block max-w-2/3 w-full h-full object-cover"
           alt="hero background"
           loading="eager"
         />
         <Image
           src={MobileImg}
-          className=" absolute inset-0 z-0 w-full h-full object-center object-cover  md:hidden"
+          className=" absolute inset-0 z-0 w-full h-full object-center object-cover  lg:hidden"
           alt="hero background"
           loading="eager"
         />
 
         {/* body */}
-        <div className=" relative z-10 w-full h-full flex flex-col justify-center items-center gap-4 lg:gap-6 md:items-start text-center md:text-left p-6 md:p-12  md:max-w-1/3 ">
-          <div className="bg-white/80 p-6 rounded-xl md:bg-transparent md:p-0 md:rounded-none  flex flex-col items-center md:items-start gap-4">
-          <h1 className=" text-4xl md:text-5xl font-semibold max-w-xs md:max-w-full">
-            Carry a 3,000 year-old <span className="text-primary">legacy</span>
-             {" "} in your pocket.
-          </h1>
+        <div className=" relative z-10 w-full h-full flex flex-col justify-center items-center gap-4  lg:gap-6 md:items-start text-center lg:text-left p-6 md:p-12  lg:max-w-1/3 ">
+          <div className="bg-white/80 p-6 rounded-xl lg:bg-transparent lg:p-0 lg:rounded-none  flex flex-col items-center lg:items-start gap-4 md:gap-8">
+            <h1 className=" text-4xl md:text-5xl font-semibold max-w-xs md:max-w-full">
+              Carry a 3,000 year-old{" "}
+              <span className="text-primary">legacy</span> in your pocket.
+            </h1>
 
-          <MainBtn
-            MainClass={" text-white bg-primary "}
-            HoverClass={"bg-primary-hover"}
+            <MainBtn
+              MainClass={" text-white bg-primary "}
+              HoverClass={"bg-primary-hover"}
             />
-            </div>
+          </div>
         </div>
       </div>
     </section>
