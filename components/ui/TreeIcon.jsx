9dd9d9a40d7ab896c268775fbb6e7edcf3263f0c
YogurtFusion <cocaniket2@gmail.com>
2026-04-25 14:06:53 +0530

    optimized about section for phone

---
+++ components/ui/TreeIcon.jsx
@@ -0,0 +1,19 @@
+export default function TreeIcon (){
+    return(
+           <svg
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M12 2l-5.5 14h11z"></path>
+            <path d="M12 16v6"></path>
+            <circle cx="12" cy="5" r="2"></circle>
+            <path d="M8 11.5l8-2"></path>
+          </svg>
+    )
+}
