ae0d20b0d1813c3a4be842692def60f2d14a5f56
YogurtFusion <cocaniket2@gmail.com>
2026-04-30 15:46:20 +0530

    added sucess section

---
+++ app/order-confirmation/page.js
@@ -0,0 +1,12 @@
+import OrderPage from '@/sections/order-confirmation/SucessPage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <OrderPage/>
+    </div>
+  )
+}
+
+export default page
